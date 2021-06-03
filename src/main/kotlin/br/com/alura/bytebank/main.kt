4bf7757b7a1b8045feddce12b0a59fc1d9203798
Luiz Moraes <lffm1994@gmail.com>
2021-06-03 20:15:59 -0300

    Módulo 2 - Lidando com exceptions com o try expression

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,26 +1,57 @@
 package br.com.alura.bytebank
 
 import br.com.alura.bytebank.modelo.Endereco
+import java.lang.ClassCastException
 
 fun main() {
     println("início main")
-    10/0
+//    try {
+//        10 / 0
+//    } catch (e: ArithmeticException) {
+//        println("ArithmeticException foi encontrada no codigo")
+//    }
+
     funcao1()
     println("fim main")
 }
 
-fun funcao1(){
+fun funcao1() {
     println("início funcao1")
-    funcao2()
+    try {
+        funcao2()
+    } catch (e: ClassCastException) {
+        println("ClassCastException foi encontrada no codigo")
+    }
     println("fim funcao1")
 }
 
 fun funcao2() {
     println("início funcao2")
-    for (i in 1..5){
+
+    for (i in 1..5) {
         println(i)
         val endereco = Any()
         endereco as Endereco
+
     }
     println("fim funcao2")
 }
+
+//try {
+//    for (i in 1..5) {
+//        println(i)
+//        val endereco = Any()
+//        endereco as Endereco
+//    }
+//} catch (e: ClassCastException) {
+//    println("ClassCastException foi encontrada no codigo")
+//}
+
+//Comportamento try dentro do for
+//        try{
+//            endereco as Endereco
+//        } catch (e: ClassCastException) {
+//            println("ClassCastException foi encontrada no codigo")
+//        }
+
+
