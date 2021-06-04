ef980622a03bf8137c0ddbec308b583723275b3b
Luiz Moraes <lffm1994@gmail.com>
2021-06-04 00:06:43 -0300

    Módulo 5 - Utilizando o safe call

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -3,14 +3,33 @@ package br.com.alura.bytebank
 import br.com.alura.bytebank.modelo.Endereco
 
 
-//Variaveis que podem receber null para é necessario utilizar ?
 fun main() {
-    //shift + f6 renomear em dois pontos
-    var enderecoNulo: Endereco? = null
-    //transforma em um endereço que não null -> !! isso torna ele valido
-    val enderecoNaoNulo: Endereco =  enderecoNulo!!
-    enderecoNaoNulo.logradouro
+    //chamada de safe call
+    var enderecoNulo: Endereco? = Endereco(logradouro = "rua vergueiro")
+    val logradouroNovo: String? = enderecoNulo?.logradouro
+    //chamada de safecall com let
+//    enderecoNulo = null
+    //função de escopo usando o safe call ? para não ter que ficar fazendo conversão usando o ?
+//    enderecoNulo?.let { endereco: Endereco ->
+//        println(endereco.logradouro)
+
+    //recrusos similares
+    enderecoNulo?.let {
+        println(it.logradouro.length)
+    }
+
+//    teste(enderecoNulo)
 }
+
+//fun teste(endereco: Endereco){}
+
+//    Chamada encadeada
+//    println(enderecoNulo?.logradouro?.length?.toUByte())
+//    shift + f6 renomear em dois pontos
+//    Variaveis que podem receber null para é necessario utilizar ?
+//    transforma em um endereço que não null -> !! isso torna ele valido
+//    val enderecoNaoNulo: Endereco =  enderecoNulo!!
+//    enderecoNaoNulo.logradouro
 
 
 
