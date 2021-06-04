b5fdb0c2581b69dc6cc095342cc7a84cf3577951
Luiz Moraes <lffm1994@gmail.com>
2021-06-03 23:38:26 -0300

    Módulo 5 - Tipos que podem receber nulo

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,82 +1,16 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.exception.SaldoInsuficienteException
 import br.com.alura.bytebank.modelo.Endereco
-import br.com.alura.bytebank.teste.testaComportamentosConta
-import testaExpressao
-import java.lang.ClassCastException
 
+
+//Variaveis que podem receber null para é necessario utilizar ?
 fun main() {
-    println("início main")
-    testaComportamentosConta()
-//    funcao1()
-    println("fim main")
+    //shift + f6 renomear em dois pontos
+    var enderecoNulo: Endereco? = null
+    //transforma em um endereço que não null -> !! isso torna ele valido
+    val enderecoNaoNulo: Endereco =  enderecoNulo!!
+    enderecoNaoNulo.logradouro
 }
 
-//recursão infinita teremos problema ela chama ela mesma sem ponto de parada
-//fun teste(){
-//    return teste()
-//}
-
-
-fun funcao1() {
-    println("início funcao1")
-    try {
-        funcao2()
-    } catch (e: SaldoInsuficienteException) {
-        println(e.printStackTrace()) //nela temos as 3 informações e.cause / e.stackTrace / e.message
-        println("ClassCastException foi encontrada no codigo")
-    }
-    println("fim funcao1")
-}
-
-fun funcao2() {
-    println("início funcao2")
-    for (i in 1..5) {
-        println(i)
-       throw SaldoInsuficienteException()
-
-    }
-    println("fim funcao2")
-}
-
-
-
-//var valorRecebido: Double = 0.0
-
-//try {
-//    // val valor: Double = entrada.toDouble() //metodo de conversão
-//    valorRecebido = entrada.toDouble()
-//    println("valor recebido: $valorRecebido")
-//} catch (e: NumberFormatException) {
-//    println("problema na conversão")
-//    e.printStackTrace()
-//}
-//
-//println("Valor recebido: $valorRecebido ")
-
-
-//    try {
-//        10 / 0
-//    } catch (e: ArithmeticException) {
-//        println("ArithmeticException foi encontrada no codigo")
-//    }
-
-//try {
-//    for (i in 1..5) {
-//        println(i)
-//        val endereco = Any()
-//        endereco as Endereco
-//    }
-//} catch (e: ClassCastException) {
-//    println("ClassCastException foi encontrada no codigo")
-//}
-
-//Comportamento try dentro do for
-//        try{
-//            endereco as Endereco
-//        } catch (e: ClassCastException) {
-//            println("ClassCastException foi encontrada no codigo")
-//        }
 
 
---
+++ src/main/kotlin/br/com/alura/bytebank/teste/testaPilha.kt
@@ -0,0 +1,74 @@
+package br.com.alura.bytebank.teste
+
+import br.com.alura.bytebank.exception.SaldoInsuficienteException
+
+fun funcao1() {
+    println("início funcao1")
+    try {
+        funcao2()
+    } catch (e: SaldoInsuficienteException) {
+        println(e.printStackTrace()) //nela temos as 3 informações e.cause / e.stackTrace / e.message
+        println("ClassCastException foi encontrada no codigo")
+    }
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("início funcao2")
+    for (i in 1..5) {
+        println(i)
+        throw SaldoInsuficienteException()
+
+    }
+    println("fim funcao2")
+}
+
+//println("início main")
+//testaComportamentosConta()
+////    funcao1()
+//println("fim main")
+
+//recursão infinita teremos problema ela chama ela mesma sem ponto de parada
+//fun teste(){
+//    return teste()
+//}
+
+
+//var valorRecebido: Double = 0.0
+
+//try {
+//    // val valor: Double = entrada.toDouble() //metodo de conversão
+//    valorRecebido = entrada.toDouble()
+//    println("valor recebido: $valorRecebido")
+//} catch (e: NumberFormatException) {
+//    println("problema na conversão")
+//    e.printStackTrace()
+//}
+//
+//println("Valor recebido: $valorRecebido ")
+
+
+//    try {
+//        10 / 0
+//    } catch (e: ArithmeticException) {
+//        println("ArithmeticException foi encontrada no codigo")
+//    }
+
+//try {
+//    for (i in 1..5) {
+//        println(i)
+//        val endereco = Any()
+//        endereco as Endereco
+//    }
+//} catch (e: ClassCastException) {
+//    println("ClassCastException foi encontrada no codigo")
+//}
+
+//Comportamento try dentro do for
+//        try{
+//            endereco as Endereco
+//        } catch (e: ClassCastException) {
+//            println("ClassCastException foi encontrada no codigo")
+//        }
+
+
