7f0b95c2adf7de6c52c94df785fdb42a8265f831
Luiz Moraes <lffm1994@gmail.com>
2021-06-04 00:22:24 -0300

    Módulo 5 - Outros recursos do null safety

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -5,21 +5,37 @@ import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
     //chamada de safe call
-    var enderecoNulo: Endereco? = Endereco(logradouro = "rua vergueiro")
+    var enderecoNulo: Endereco? = Endereco(logradouro = "rua vergueiro", complemento = "prédio")
     val logradouroNovo: String? = enderecoNulo?.logradouro
+
+    //recursos similares safecall com let
+    enderecoNulo?.let {
+        println(it.logradouro.length)
+        //Elvis Operator é uma sintax shugar
+        val tamanhoComplemento: Int = it.complemento?.length ?: throw IllegalStateException("Complemento não pode ser vazio")
+//        val tamanhoComplemento: Int = it.complemento?.length ?: 0 //devolve 0 caso seja vazio ou length caso não seja null
+        println(tamanhoComplemento)
+    }
+    teste("")
+//    teste(1)
+}
+
+//Safe Cast
+//evitamos exception
+fun teste(valor:Any){
+    val numero: Int? = valor as? Int
+    println(numero)
+}
+
     //chamada de safecall com let
 //    enderecoNulo = null
     //função de escopo usando o safe call ? para não ter que ficar fazendo conversão usando o ?
 //    enderecoNulo?.let { endereco: Endereco ->
 //        println(endereco.logradouro)
 
-    //recrusos similares
-    enderecoNulo?.let {
-        println(it.logradouro.length)
-    }
 
 //    teste(enderecoNulo)
-}
+
 
 //fun teste(endereco: Endereco){}
 
